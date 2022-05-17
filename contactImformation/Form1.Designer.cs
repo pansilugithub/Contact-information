d3f8f95a7de39c164f1c5a400d42ca04db07ac61
pansilugithub <101410227+pansilugithub@users.noreply.github.com>
2022-05-17 20:17:04 +0530

    added project

---
+++ contactImformation/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace contactImformation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbaddress = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txttelephone = new System.Windows.Forms.TextBox();
+            this.txtmobile = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.cbcountry = new System.Windows.Forms.ComboBox();
+            this.btnnew = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(222, 45);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(233, 42);
+            this.txtname.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address";
+            // 
+            // rbaddress
+            // 
+            this.rbaddress.Location = new System.Drawing.Point(222, 116);
+            this.rbaddress.Name = "rbaddress";
+            this.rbaddress.Size = new System.Drawing.Size(257, 155);
+            this.rbaddress.TabIndex = 2;
+            this.rbaddress.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(604, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 36);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Telephone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(604, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 36);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Mobile";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(627, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 36);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(618, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 36);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Country";
+            // 
+            // txttelephone
+            // 
+            this.txttelephone.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttelephone.Location = new System.Drawing.Point(764, 45);
+            this.txttelephone.Name = "txttelephone";
+            this.txttelephone.Size = new System.Drawing.Size(275, 42);
+            this.txttelephone.TabIndex = 0;
+            // 
+            // txtmobile
+            // 
+            this.txtmobile.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmobile.Location = new System.Drawing.Point(764, 122);
+            this.txtmobile.Name = "txtmobile";
+            this.txtmobile.Size = new System.Drawing.Size(275, 42);
+            this.txtmobile.TabIndex = 0;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.Location = new System.Drawing.Point(764, 193);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(275, 42);
+            this.txtemail.TabIndex = 0;
+            // 
+            // cbcountry
+            // 
+            this.cbcountry.FormattingEnabled = true;
+            this.cbcountry.Location = new System.Drawing.Point(764, 267);
+            this.cbcountry.Name = "cbcountry";
+            this.cbcountry.Size = new System.Drawing.Size(275, 24);
+            this.cbcountry.TabIndex = 3;
+            this.cbcountry.SelectedIndexChanged += new System.EventHandler(this.cbcountry_SelectedIndexChanged);
+            // 
+            // btnnew
+            // 
+            this.btnnew.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnnew.Location = new System.Drawing.Point(384, 334);
+            this.btnnew.Name = "btnnew";
+            this.btnnew.Size = new System.Drawing.Size(156, 47);
+            this.btnnew.TabIndex = 4;
+            this.btnnew.Text = "New";
+            this.btnnew.UseVisualStyleBackColor = true;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(571, 334);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(156, 47);
+            this.btnsave.TabIndex = 4;
+            this.btnsave.Text = "save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.Location = new System.Drawing.Point(746, 334);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(156, 47);
+            this.btnupdate.TabIndex = 4;
+            this.btnupdate.Text = "update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            // 
+            // btndelete
+            // 
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.Location = new System.Drawing.Point(929, 334);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(156, 47);
+            this.btndelete.TabIndex = 4;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 394);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1042, 206);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 612);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnnew);
+            this.Controls.Add(this.cbcountry);
+            this.Controls.Add(this.rbaddress);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtmobile);
+            this.Controls.Add(this.txttelephone);
+            this.Controls.Add(this.txtname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rbaddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txttelephone;
+        private System.Windows.Forms.TextBox txtmobile;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.ComboBox cbcountry;
+        private System.Windows.Forms.Button btnnew;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ contactImformation/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contactImformation
+{
+    public partial class Form1 : Form
+    {
+        ContactDB_pansiluEntities1 db= new ContactDB_pansiluEntities1();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.tblcontacts.ToList();
+            try
+            {
+                using(ContactDB_pansiluEntities1 db =new ContactDB_pansiluEntities1 ()) 
+                {
+                    cbcountry.DataSource = db.tblCountries.ToList();
+                    cbcountry.ValueMember = "CountryID";
+                    cbcountry.DisplayMember = "Country";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        void clear()
+        {
+            txtname.Text = txttelephone.Text = txtmobile.Text = txtemail.Text = "";
+        }
+
+        private void cbcountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
